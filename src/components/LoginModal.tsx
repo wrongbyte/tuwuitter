d03cba21c5ba00046fcd29eecdbc9048f658556e
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-07-11 09:47:14 -0300

    feat(login page): add background to login modal

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -8,34 +8,37 @@ export default function LoginModal({
   setOpenModal: Dispatch<SetStateAction<boolean>>;
 }) {
   return (
-    <div className="login-modal">
-      <div className="login-wrapper">
-        <div className="close-x" onClick={() => setOpenModal(false)}>
-          x
-        </div>
-        <img className="img-modal-login" src="twitter-xxl.png" alt="" />
-        <p className="join-phrase-login">Join Tuwuitter today :3 </p>
-        <button className="button-white-modal-login">
-          Sign up with Google
-        </button>
-        <button className="button-white-modal-login font-bold">
-          Sign up with Apple
-        </button>
-        <p className="or-text">or</p>
-        <button className="button-white-modal-login font-bold">
-          Sign up with phone or email
-        </button>
+    <>
+      <div className="login-modal">
+        <div className="login-wrapper">
+          <div className="close-x" onClick={() => setOpenModal(false)}>
+            x
+          </div>
+          <img className="img-modal-login" src="twitter-xxl.png" alt="" />
+          <p className="join-phrase-login">Join Tuwuitter today :3 </p>
+          <button className="button-white-modal-login">
+            Sign up with Google
+          </button>
+          <button className="button-white-modal-login font-bold">
+            Sign up with Apple
+          </button>
+          <p className="or-text">or</p>
+          <button className="button-white-modal-login font-bold">
+            Sign up with phone or email
+          </button>
 
-        <p className="policy">
-          By signing up, you agree to the{' '}
-          <span className="highlight">Terms of Service</span> and{' '}
-          <span className="highlight">Privacy Policy</span>, including{' '}
-          <span className="highlight">Cookie Use</span>.
-        </p>
-        <p className="policy">
-          Have an account already? <span className="highlight">Log in</span>
-        </p>
+          <p className="policy">
+            By signing up, you agree to the{' '}
+            <span className="highlight">Terms of Service</span> and{' '}
+            <span className="highlight">Privacy Policy</span>, including{' '}
+            <span className="highlight">Cookie Use</span>.
+          </p>
+          <p className="policy">
+            Have an account already? <span className="highlight">Log in</span>
+          </p>
+        </div>
       </div>
-    </div>
+      <div className="modalBackground"></div>
+    </>
   );
 }
