b1e2f5fd32a9babe0e451fc33b2062286a5173be
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-07-13 16:46:54 -0300

    feat(register page): add step one modal

--- src/components/ModalLayout.tsx
+++ src/components/ModalLayout.tsx
@@ -1,26 +1,13 @@
-export default function ModalLayout() {
+import { ReactNode } from 'react';
+
+export default function ModalLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <main className="login-page-main flex justify-center">
         <div className="login-modal">
           <div className="login-wrapper">
             <div className="close-x">x</div>
-            <p className="step-modal">Step 1 of 5 </p>
-            <p className="create-account">Create your account</p>
-            <form>
-              <input
-                type="search"
-                className="login-input-register"
-                placeholder="Name"
-              />
-            </form>
-            <form>
-              <input
-                type="search"
-                className="login-input-register"
-                placeholder="Phone"
-              />
-            </form>
+            {children}
           </div>
         </div>
         <div className="modalBackground"></div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import LoginPage from './pages/LoginPage';
 import ModalLayout from './components/ModalLayout';
+import RegisterAccount from './components/modals/RegisterAccount';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/test" element={<ModalLayout />} />
+        <Route path="/register" element={<RegisterAccount />} />
       </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
