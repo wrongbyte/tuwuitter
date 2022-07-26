c3e9d149b02db7bad582f181c050c82e8d81ef33
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-07-26 17:32:01 -0300

    refactor: refactor queries

--- src/graphql/queryType.ts
+++ src/graphql/queryType.ts
@@ -1,12 +1,10 @@
 import { GraphQLObjectType } from 'graphql';
-import { VersionQuery } from '../modules/user/queries/testQuery';
-import { FindUsers } from '../modules/user/queries';
+import * as userQueries from '../modules/user/queries';
 
 export const QueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root of queries',
   fields: () => ({
-    VersionQuery,
-    FindUsers,
+    ...userQueries,
   }),
 });
--- src/modules/user/queries/index.ts
+++ src/modules/user/queries/index.ts
@@ -1,1 +1,2 @@
 export * from './findUsers';
+export * from './testQuery';
