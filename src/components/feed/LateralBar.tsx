ced9cb22d4bed7f0b1423796fb12971d6d8b2fa5
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-07-29 17:44:21 -0300

    feat: write tweet modal layout (#4)

--- src/components/feed/LateralBar.tsx
+++ src/components/feed/LateralBar.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import '../../styles/global.css';
+import NewTweetModal from './NewTweetModal';
 import { ReactComponent as HomeIcon } from '../../assets/home.svg';
 import { ReactComponent as SearchIcon } from '../../assets/search.svg';
 import { ReactComponent as NotificationsIcon } from '../../assets/notifications.svg';
@@ -10,24 +12,34 @@ import { ReactComponent as MoreIcon } from '../../assets/more.svg';
 import { ReactComponent as WriteTweetIcon } from '../../assets/writetweet.svg';
 
 export default function LateralBar() {
+  const [openNewTweetModal, setOpenTweetModal] = useState(false);
+
   return (
-    <aside className="lateralbar-wrapper">
-      <div className="lateralbar-icons">
-        <img className="twitter-icon-lateralbar" src="twitter-xxl.png"></img>
+    <>
+      {openNewTweetModal !== false && (
+        <NewTweetModal setOpenTweetModal={setOpenTweetModal} />
+      )}
+      <aside className="lateralbar-wrapper">
+        <div className="lateralbar-icons">
+          <img className="twitter-icon-lateralbar" src="twitter-xxl.png"></img>
 
-        <HomeIcon className="svg-small" />
-        <SearchIcon className="svg-small" />
-        <NotificationsIcon className="svg-small" />
-        <MessagesIcon className="svg-small" />
-        <SavedItemsIcon className="svg-small" />
-        <ListsIcon className="svg-small" />
-        <UserProfileIcon className="svg-small" />
-        <MoreIcon className="svg-small" />
+          <HomeIcon className="svg-small" />
+          <SearchIcon className="svg-small" />
+          <NotificationsIcon className="svg-small" />
+          <MessagesIcon className="svg-small" />
+          <SavedItemsIcon className="svg-small" />
+          <ListsIcon className="svg-small" />
+          <UserProfileIcon className="svg-small" />
+          <MoreIcon className="svg-small" />
 
-        <div className="blue-circle">
-          <WriteTweetIcon className="svg-small" />
+          <div className="blue-circle cursor-pointer">
+            <WriteTweetIcon
+              className="svg-small"
+              onClick={() => setOpenTweetModal(true)}
+            />
+          </div>
         </div>
-      </div>
-    </aside>
+      </aside>
+    </>
   );
 }
