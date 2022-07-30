a302dbdc1fefff87fec28352e661a4712807fb5e
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-07-30 15:47:10 -0300

    feat(login): add validation in login form (#5)

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,13 +1,41 @@
 import '../styles/global.css';
 import '../styles/login.css';
 import { Dispatch, SetStateAction } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Link } from 'react-router-dom';
+import { object, string, TypeOf } from 'zod';
+
+const loginSchema = object({
+  email: string().min(1, 'Provide your email').email('Invalid email'),
+  password: string()
+    .min(1, 'Provide your password')
+    .max(32, 'Password must be less than 32 characters'),
+});
+
+type ILogin = TypeOf<typeof loginSchema>;
 
 export default function LoginModal({
   setOpenLoginModal,
 }: {
   setOpenLoginModal: Dispatch<SetStateAction<boolean>>;
 }) {
+  const defaultValues: ILogin = {
+    email: '',
+    password: '',
+  };
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<ILogin>({
+    resolver: zodResolver(loginSchema),
+    defaultValues,
+  });
+
+  const onSubmitHandler: SubmitHandler<ILogin> = (values: ILogin) => {
+    console.log(values);
+  };
   return (
     <>
       <div className="login-modal">
@@ -17,11 +45,40 @@ export default function LoginModal({
           </div>
           <img className="img-modal-login" src="twitter-xxl.png" alt="" />
           <p className="join-phrase-login">Sign in to Tuwuitter :3 </p>
-          <form className="flex flex-col gap-10">
-            <input type="text" className="login-input" placeholder="Username" />
-            <input type="text" className="login-input" placeholder="Password" />
+
+          <form
+            className="flex flex-col gap-10"
+            onSubmit={handleSubmit(onSubmitHandler)}
+          >
+            <input
+              type="text"
+              className="login-input"
+              placeholder="Email"
+              {...register('email')}
+            />
+            {errors && (
+              <span className="text-red-500 -mt-3 -mb-3 text-center">
+                {errors.email?.message}
+              </span>
+            )}
+            <input
+              type="password"
+              className="login-input"
+              placeholder="Password"
+              {...register('password')}
+            />
+            {errors && (
+              <span className="text-red-500 -mt-3 -mb-3 text-center">
+                {errors.password?.message}
+              </span>
+            )}
+            <input
+              type="submit"
+              className="button-white-modal-login font-bold cursor-pointer"
+              value="Next"
+            />
           </form>
-          <button className="button-white-modal-login font-bold">Next</button>
+
           <button className="button-black-modal-login font-bold">
             Forgot password?
           </button>
