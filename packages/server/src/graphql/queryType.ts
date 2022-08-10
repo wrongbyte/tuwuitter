bef6403a51551a4e8680acc573fb04a992230cc9
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-08-09 22:05:08 -0300

    test: changing return of type

--- packages/server/src/graphql/queryType.ts
+++ packages/server/src/graphql/queryType.ts
@@ -2,7 +2,7 @@ import { GraphQLNonNull, GraphQLObjectType } from 'graphql';
 import { nodeField, nodesField } from './typeRegister';
 import * as userQueries from '../modules/user/queries';
 import { UserConnection, UserType } from '../modules/user/userType';
-import { connectionArgs } from '@entria/graphql-mongo-helpers';
+import { connectionArgs, withFilter } from '@entria/graphql-mongo-helpers';
 import * as UserLoader from '../modules/user/UserLoader';
 
 export const QueryType = new GraphQLObjectType({
