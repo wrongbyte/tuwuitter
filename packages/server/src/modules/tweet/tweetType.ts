5a5b8e36814f8ae2d0b9d63a08d9fa4b84065100
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-08-14 16:39:44 -0300

    refactor: improve tweet mutations

--- packages/server/src/modules/tweet/tweetType.ts
+++ packages/server/src/modules/tweet/tweetType.ts
@@ -8,14 +8,20 @@ import { registerTypeLoader, nodeInterface } from '../../graphql/typeRegister';
 import { connectionDefinitions, globalIdField } from 'graphql-relay';
 import { Tweet } from './tweetModel';
 import { load } from './TweetLoader';
+import { UserType } from '../user/userType';
+import * as UserLoader from '../user/UserLoader';
+import { UserModel } from '../user/userModel';
 
 export const TweetType = new GraphQLObjectType<Tweet>({
   name: 'Tweet',
   fields: () => ({
     id: globalIdField('Tweet'),
     author: {
-      type: new GraphQLNonNull(GraphQLString),
-      resolve: (tweet) => tweet.author,
+      type: UserType,
+      resolve: async (tweet, _, context) => {
+        const user = await UserModel.findById(tweet.author);
+        return await UserLoader.load(context, user._id);
+      },
     },
     content: {
       type: new GraphQLNonNull(GraphQLString),
