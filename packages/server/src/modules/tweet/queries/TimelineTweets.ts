f277007675c8e12576a9bd5094f916530e313f8a
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-08-16 21:11:36 -0300

    refactor: remove useless code

--- packages/server/src/modules/tweet/queries/TimelineTweets.ts
+++ packages/server/src/modules/tweet/queries/TimelineTweets.ts
@@ -2,13 +2,12 @@ import { withFilter, connectionArgs } from '@entria/graphql-mongo-helpers';
 import { GraphQLFieldConfig, GraphQLNonNull, GraphQLString } from 'graphql';
 import { UserModel } from '../../user/userModel';
 import * as TweetLoader from '../TweetLoader';
-import { TweetModel } from '../tweetModel';
-import { TweetConnection, TweetType } from '../tweetType';
+import { TweetConnection } from '../tweetType';
 
 export const findTimelineTweets: GraphQLFieldConfig<any, any, any> = {
   type: new GraphQLNonNull(TweetConnection.connectionType),
   args: { ...connectionArgs },
-  resolve: async (user, args, context) => {
+  resolve: async (_, args, context) => {
     const currentUser = await UserModel.findById(context.user._id);
     return await TweetLoader.loadAll(
       context,
