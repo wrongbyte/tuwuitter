ddae8f0c019a6f1112428542402e36d4e12e9016
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-08-19 21:34:30 -0300

    test: test page error modal

--- packages/client/src/pages/Test.tsx
+++ packages/client/src/pages/Test.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import ErrorModal from '../components/ErrorModal';
+import type { TestQuery$data } from './__generated__/TestQuery.graphql';
 const { graphql, useLazyLoadQuery } = require('react-relay');
 
 export default function Test() {
+  const [errorStatus, setErrorStatus] = useState<boolean | string>(false);
   const data = useLazyLoadQuery(
     graphql`
       query TestQuery($id: ID!) {
@@ -14,8 +17,11 @@ export default function Test() {
         }
       }
     `,
-    { id: 'VXNlcjo2MmRmM2UyZDE3ODc2YWI0MjdlNmJlMmY=' }
-  );
+    { id: 'VXNlcjo2MmRmM2UyZDE3ODc2YWI0MjdlNmJlMnY=' }
+  ) as TestQuery$data;
 
-  return <ErrorModal phrase="Invalid token."></ErrorModal>;
+  return (
+    <h1 className="text-white">{data.node?.displayName}</h1>
+    // <ErrorModal phrase="Invalid token." setErrorStatus={setErrorStatus}></ErrorModal>
+  );
 }
