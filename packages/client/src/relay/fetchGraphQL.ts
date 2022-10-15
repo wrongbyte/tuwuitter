4d1ae32132ef994b7afcdd8a728170bcdc2099f8
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-10-15 16:55:45 -0300

    fix: fix fetch for prd

--- packages/client/src/relay/fetchGraphQL.ts
+++ packages/client/src/relay/fetchGraphQL.ts
@@ -40,7 +40,7 @@ export const setupSubscription = (request: RequestParameters, variables: Variabl
 export const fetchGraphQL = async (query: string, variables: Variables) => {
   const token = localStorage.getItem('ACCESS_TOKEN');
 
-  const response = await fetch('http://localhost:3001/graphql', {
+  const response = await fetch('/graphql', {
     method: 'POST',
     headers: {
       'Access-Control-Allow-Origin': '*',
